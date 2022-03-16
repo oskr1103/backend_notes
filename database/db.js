0ef361c78700a462c25938f4b82e0d186b69b8bb
Isai Colina <isaicolinapinto@gmail.com>
2022-03-16 17:05:27 -0500

    change db host

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("rehobot_notas", "rehobot_notasUser", "Converse04?", {
-  host: "rehobot.co",
+  host: "190.8.176.248",
   dialect: "mysql",
+  port: 3306,
 });
 
 export default db;
